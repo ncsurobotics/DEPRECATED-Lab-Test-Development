ca9acab9e66b31ad79a875c835761337151df544
Josh Smith <jrsmit23@ncsu.edu>
2015-09-24 08:10:45 -0400

    intermediate changes

--- videoray_speed_controller/videoray_speed_controller.ino
+++ videoray_speed_controller/videoray_speed_controller.ino
@@ -1,4 +1,6 @@
 int led = 5;
+int potPin = A2;
+int val = 0; 
 
 uint16_t loop_time = 32768; // about 51Hz
 uint16_t on_time = 1050;
@@ -6,9 +8,11 @@ uint16_t off_time = loop_time-on_time;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as a low output.
+  // initialize the digital pin as an output.
   pinMode(led, OUTPUT);    
   digitalWrite(led, LOW);
+  
+  
 }
 
 // the loop routine runs over and over again forever:
@@ -23,9 +27,12 @@ void loop() {
     digitalWrite(led, LOW);
     delayMicroseconds(off_time);
     i++;
+    val = analogRead(potPin);
+    val = map(val, 0, 1023, 0,180);
+    delay(1);
   }
   
-  on_time = 1250;
+  on_time = 1350;
   off_time = loop_time-on_time;
   while(1) {
    // start of loop. set output high
